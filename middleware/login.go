bb11e94469d9533ddadb4863ee6834796c7680a7
Matvey Fedorov <mfgolden@yandex.ru>
2025-05-16 16:00:01 +0300

    login func

---
+++ middleware/login.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(c fiber.Ctx) error {
+	type LoginInput struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var input LoginInput
+	if err := c.Bind().Body(&input); err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	user, err := FindUserByEmail(input.Email)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, "Error findind user by email")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, "Password doesn't match the hash")
+	}
+
+	token, err := GenerateJWT(user.ID)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(fiber.Map{"token": token})
+}
