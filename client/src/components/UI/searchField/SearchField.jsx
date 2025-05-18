e3587c07a225d47c629318d1d92c2b990e47bb89
Segun228 <segunperkele@gmail.com>
2025-05-18 10:42:52 +0300

    correcting some chrome bugs

--- client/src/components/UI/searchField/SearchField.jsx
+++ client/src/components/UI/searchField/SearchField.jsx
@@ -2,9 +2,9 @@ import styles from "./searchField.module.css"
 import glass from "./../../../assets/vectors/glass_grey.svg"
 const SearchField = () => {
     return (<>
-    <div className={styles.wrapper}>
-        <input type="search" placeholder="Search here..." className={styles.field} />
-        <img draggable="false" src={glass} alt="glass icon" className={styles.glass} />
+    <div className={styles.wrapper} style={{minWidth: "1em"}}>
+        <input type="search" placeholder="Search here..." className={styles.field} style={{minWidth: "1em"}}/>
+        <img draggable="false" src={glass} alt="glass icon" className={styles.glass} style={{minWidth: "1em"}}/>
     </div>
     </>);
 }
--- client/src/pages/pageComponents/singlePrepodPage/SinglePrepodPage.jsx
+++ client/src/pages/pageComponents/singlePrepodPage/SinglePrepodPage.jsx
@@ -120,13 +120,13 @@ const SinglePrepodPage = () => {
                                     </SwiperSlide>
                                 </Swiper>
                                 <div className={styles.swiperButtonPrev}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                                    <svg className={styles.swiperSVG} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
                                         <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
                                     </svg>
                                 </div>
 
                                 <div className={styles.swiperButtonNext}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
+                                    <svg className={styles.swiperSVG} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
                                         <path d="M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6z"/>
                                     </svg>
                                 </div>
