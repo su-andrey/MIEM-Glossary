3957b95f35d034b8c9947838e39036608e3d5fa2
Швырев Андрей Андреевич <aashvyrev@edu.hse.ru>
2025-05-19 19:10:02 +0300

    advanced find logic

--- handlers/personFind.go
+++ handlers/personFind.go
@@ -8,9 +8,10 @@ import (
 	"time"
 )
 
-// Краткая документация
-// Вызов: response, err := sendRequest("Литвиненко", "Алексей", "Михайлович")
-// if err != nil {
+// Краткая документация. Пример вызова. Необязательно заполнять все поля. Главное >2 символов и не нарушать порядок: Ф, И, О
+// func main() {
+// 	response, err := get_link_by_name("Литвиненко Алексей Миха")
+// 	if err != nil {
 // 		fmt.Printf("%v\n", err)
 // 		return
 // 	}
@@ -18,14 +19,19 @@ import (
 // 		fmt.Printf("The teacher doesn't seem to exist")
 // 		return
 // 	}
-//  fmt.Println(response)
+// 	fmt.Println(response)
+// }
 
-func get_link_by_name(surname string, name string, patronic string) (string, error) {
-	url := fmt.Sprintf("https://www.hse.ru/org/persons/?search_person=%s+%s+%s", name, surname, patronic) // базовый url поиска
+func get_link_by_name(fullname string) (string, error) {
+	if len(fullname) < 2 {
+		return "", fmt.Errorf("Invalid input data") // меньше 2 символов не ищет сайт ВШЭ
+	}
+	parts := strings.Fields(fullname)
+	url := fmt.Sprintf("https://www.hse.ru/org/persons/?search_person=%s", strings.Join(parts[:min(3, len(parts))], "+")) // базовый url поиска
+	// Если введены не все поля, то суммаризируем имеющиеся, ищем по ним
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 	} // настраиваем клиент
-
 	req, err := http.NewRequest("GET", url, nil) // отправляем запрос, получаем ответ
 	if err != nil {                              // отрабатываем самые популярные ошибки
 		return "", fmt.Errorf("Request creation error: %v", err)
@@ -52,5 +58,16 @@ func get_link_by_name(surname string, name string, patronic string) (string, err
 		}
 	}()
 	id := strings.Split(strings.Split(string(body), "background-image: url(/org/persons/cimage/")[1], ")")[0]
-	return fmt.Sprintf("https://www.hse.ru/org/persons/%s", id), nil
+	prepod_link := fmt.Sprintf("https://www.hse.ru/org/persons/%s", id)
+	tex, err := http.NewRequest("GET", prepod_link, nil)
+	check, err := client.Do(tex)
+	if err != nil {
+		print(1)
+		return "", fmt.Errorf("Error during control request")
+	}
+	defer check.Body.Close()
+	if check.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Error: bad link. Status: %d ", check.StatusCode)
+	}
+	return prepod_link, nil
 }
