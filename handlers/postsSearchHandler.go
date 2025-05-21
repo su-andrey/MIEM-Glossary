95c938f5b1a78d2f3eb71f0f6a677c12c28c95ad
Matvey Fedorov <mfgolden@yandex.ru>
2025-05-21 12:31:20 +0300

    trimming spaces from strings start and end

--- handlers/postsSearchHandler.go
+++ handlers/postsSearchHandler.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v3"
 	"github.com/su-andrey/kr_aip/services"
 )
 
 func SearchPosts(c fiber.Ctx) error {
-	str := c.Params("str")
+	str := strings.TrimSpace(c.Params("str"))
 
 	opts := &services.Options{
 		Condition: &services.Condition{
