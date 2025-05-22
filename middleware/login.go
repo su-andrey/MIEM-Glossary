55a1be6065c4e7c8e05540da0fe41ef1b2696d70
Matvey Fedorov <mfgolden@yandex.ru>
2025-05-22 16:06:22 +0300

    adding auth validation

--- middleware/login.go
+++ middleware/login.go
@@ -2,19 +2,20 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/su-andrey/kr_aip/config"
 	"github.com/su-andrey/kr_aip/services"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func Login(c fiber.Ctx) error {
-	type LoginInput struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+	var input AuthInput
 
-	var input LoginInput
 	if err := c.Bind().Body(&input); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "неверный формат данных")
+	}
+
+	if err := config.Validator.Struct(&input); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "ошибка валидации")
 	}
 
 	user, err := services.GetUserByEmail(c.Context(), input.Email)
