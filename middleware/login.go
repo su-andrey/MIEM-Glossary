5e0cbad822c552435e55160d7954f64c56097121
Matvey Fedorov <mfgolden@yandex.ru>
2025-05-22 12:57:11 +0300

    updating loging

--- middleware/login.go
+++ middleware/login.go
@@ -14,22 +14,22 @@ func Login(c fiber.Ctx) error {
 
 	var input LoginInput
 	if err := c.Bind().Body(&input); err != nil {
-		return fiber.ErrBadRequest
+		return fiber.NewError(fiber.StatusBadRequest, "неверный формат данных")
 	}
 
 	user, err := services.GetUserByEmail(c.Context(), input.Email)
 
 	if err != nil {
-		return fiber.NewError(fiber.StatusUnauthorized, "error findind user by email")
+		return fiber.NewError(fiber.StatusUnauthorized, "ошибка поиска пользователя")
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
-		return fiber.NewError(fiber.StatusUnauthorized, "password doesn't match the hash: "+user.Password+"\n"+input.Password)
+		return fiber.NewError(fiber.StatusUnauthorized, "неверный пароль")
 	}
 
 	token, err := GenerateJWT(user.ID, user.IsAdmin)
 	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, "error generating token")
+		return fiber.NewError(fiber.StatusInternalServerError, "ошибка генерации токена")
 	}
 
 	return c.JSON(fiber.Map{"token": token})
