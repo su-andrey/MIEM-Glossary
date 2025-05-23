3a32e2f7a2742424e1b7bef050cdf4e19aef46bc
Matvey Fedorov <mfgolden@yandex.ru>
2025-05-23 18:23:53 +0300

    added limit on photos amount on each post

--- handlers/photoHandler.go
+++ handlers/photoHandler.go
@@ -13,6 +13,16 @@ func UploadPostPhotos(c fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "неверный формат данных")
 	}
 	files := form.File["photos[]"]
+	// Проверяем сколько уже фотографий у поста
+	post, err := services.GetPostByID(c.Context(), postID)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "ошибка получения фотографий поста")
+	}
+
+	if len(post.Photos)+len(files) > 10 {
+		return fiber.NewError(fiber.StatusBadRequest, "на посте не может быть более 10 фотографий")
+	}
+
 	cfg := config.LoadConfig()
 
 	urls, err := services.UploadPostPhotos(c.Context(), cfg, postID, files)
