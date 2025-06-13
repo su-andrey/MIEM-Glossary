4c86377263496a3012fdc959f12c9cb232f1f743
Matvey Fedorov <mfgolden@yandex.ru>
2025-06-13 22:41:12 +0300

    fixing emems placeholders

--- client/src/custom hooks/helpers/getRandomImagePath.js
+++ client/src/custom hooks/helpers/getRandomImagePath.js
@@ -2,5 +2,5 @@
 export default function getRandomImagePath() {
     const totalImages = 27;
     const randomIndex = Math.floor(Math.random() * totalImages) + 1;
-    return `/memes/${randomIndex}.jpeg`;
+    return `/assets/memes/${randomIndex}.jpeg`;
 }
